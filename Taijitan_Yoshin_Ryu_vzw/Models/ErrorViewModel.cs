d022b7d51dd42b7f9945a7b92f1708a77f08dbe0
hansv <hansv@DESKTOP-FGSH4KF>
2019-02-19 10:43:52 +0100

    Add project files.

---
+++ Taijitan_Yoshin_Ryu_vzw/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Taijitan_Yoshin_Ryu_vzw.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

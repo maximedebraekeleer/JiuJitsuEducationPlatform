2542d7a54d2dec4345622daf277b322fa2eab7df
Daan Van Vooren <daan.vanvooren.y1502@student.hogent.be>
2019-02-21 17:14:22 +0100

    Aanmaken/Implementeren klasse Lesgroep

---
+++ Taijitan_Yoshin_Ryu_vzw/Models/Domain/Lesgroep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taijitan_Yoshin_Ryu_vzw.Models.Domain
+{
+    public class Lesgroep
+    {
+        public string Groepsnaam { get; set; }
+
+        public Lesgroep(string groepsnaam)
+        {
+            Groepsnaam = groepsnaam;
+        }
+    }
+}

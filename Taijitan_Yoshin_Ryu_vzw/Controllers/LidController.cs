887f1831672bff592188aafa1fcd3fa705d7922b
Michael Vermassen <michael.vermassen.w3812@student.hogent.be>
2019-02-22 19:56:38 +0100

    LidController, ILidRep en EditVM toegevoegd

---
+++ Taijitan_Yoshin_Ryu_vzw/Controllers/LidController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Taijitan_Yoshin_Ryu_vzw.Models.Domain;
+using Taijitan_Yoshin_Ryu_vzw.Models.LidViewModels;
+
+namespace Taijitan_Yoshin_Ryu_vzw.Controllers {
+    public class LidController : Controller {
+        private readonly ILidRepository _lidRepository;
+
+        public LidController(ILidRepository lidRepository) {
+            _lidRepository = lidRepository;
+        }
+
+        public IActionResult Index(Lid lid) {
+            lid = _lidRepository.GetByEmail(HttpContext.Session.Id);
+            return View(lid);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string email) {
+            Lid lid = _lidRepository.GetByEmail(email);
+            if (lid == null)
+                return NotFound();
+            return View(new EditViewModel(lid));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditViewModel levm, string email) { //levm = lidEditViewModel (verkorte versie)
+            if (ModelState.IsValid) {
+                Lid lid = null;
+
+                try {
+                    lid = _lidRepository.GetByEmail(email);
+
+                    //map LidViewModel to Lid
+                    lid.Naam = levm.Naam;
+                    lid.Voornaam = levm.Voornaam;
+                    lid.GeboorteDatum = levm.GeboorteDatum;
+                    lid.Straat = levm.Straat;
+                    lid.Gemeente = levm.Gemeente;
+                    lid.HuisNummer = levm.HuisNummer;
+                    lid.Postcode = levm.PostCode;
+                    lid.TelefoonNummer = levm.TelefoonNummer;
+                    lid.Email = levm.Email;
+
+                    _lidRepository.SaveChanges();
+                    TempData["message"] = $"Je hebt je gegevens succesvol geupdated {lid.Naam}!";
+                }
+                catch (Exception e) {
+                    ModelState.AddModelError("", e.Message);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nameof(Edit), levm);
+        }
+    }
+}
---
+++ Taijitan_Yoshin_Ryu_vzw/Models/Domain/ILidRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taijitan_Yoshin_Ryu_vzw.Models.Domain {
+    public interface ILidRepository {
+        Lid GetByEmail(string email);
+        void Add(Lid lid);
+        void Delete(Lid lid);
+        void SaveChanges();
+    }
+}

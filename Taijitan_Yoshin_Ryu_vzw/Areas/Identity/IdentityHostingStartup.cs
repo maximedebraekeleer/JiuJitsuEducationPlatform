1a4729fab6b7c22f8320d57d752e1dca6355813e
Maxime De Braekeleer <maxime.debraekeleer.y7196@student.hogent.be>
2019-02-26 11:36:01 +0100

    Login partial view (razor page)

---
+++ Taijitan_Yoshin_Ryu_vzw/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Taijitan_Yoshin_Ryu_vzw.Data;
+
+[assembly: HostingStartup(typeof(Taijitan_Yoshin_Ryu_vzw.Areas.Identity.IdentityHostingStartup))]
+namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

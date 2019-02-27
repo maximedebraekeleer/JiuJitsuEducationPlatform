edc1bdd2b7e3d50222c2640aaa19648336031738
Maxime De Braekeleer <maxime.debraekeleer.y7196@student.hogent.be>
2019-02-27 22:38:07 +0100

    Revert "Werkende login + uitwerken layout"
    This reverts commit 3903ed984657e7857c65138672238079ec7f8a6d.

--- Taijitan_Yoshin_Ryu_vzw/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Taijitan_Yoshin_Ryu_vzw/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -20,8 +20,6 @@ namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage
 
         public static string PersonalData => "PersonalData";
 
-        public static string TwoFactorAuthentication => "TwoFactorAuthentication";
-
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
@@ -30,11 +28,8 @@ namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage
 
         public static string DeletePersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DeletePersonalData);
 
-        public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
-
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
 
-        public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
     
         public static string PageNavClass(ViewContext viewContext, string page)
         {

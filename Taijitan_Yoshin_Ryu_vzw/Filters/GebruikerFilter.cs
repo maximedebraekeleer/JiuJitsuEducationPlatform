ab77410fdb194f38a9378945bd27132f1a4052c4
Michael Vermassen <michael.vermassen.w3812@student.hogent.be>
2019-03-11 16:53:24 +0100

    Add gebruikerFilter

---
+++ Taijitan_Yoshin_Ryu_vzw/Filters/GebruikerFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Taijitan_Yoshin_Ryu_vzw.Models.Domain;
+
+namespace Taijitan_Yoshin_Ryu_vzw.Filters {
+    public class GebruikerFilter : ActionFilterAttribute {
+
+        private readonly IGebruikerRepository _gebruikerRepository;
+
+        public GebruikerFilter(IGebruikerRepository gebruikerRepository) {
+            _gebruikerRepository = gebruikerRepository;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context) {
+            Gebruiker gebruiker;
+            if (context.HttpContext.User.Identity.IsAuthenticated) {
+                gebruiker = _gebruikerRepository.GetByEmail(context.HttpContext.User.Identity.Name);
+
+                context.ActionArguments["gebruiker"] = gebruiker;
+            }
+            base.OnActionExecuting(context);
+
+        }
+    }
+}

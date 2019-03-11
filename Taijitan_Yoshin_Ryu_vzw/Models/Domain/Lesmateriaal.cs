1b9328404955e43f48ea8813ed9b8d367eb8e206
Maxime De Braekeleer <maxime.debraekeleer.y7196@student.hogent.be>
2019-03-11 13:38:46 +0100

    Graad, Lesmateriaal en LesmateriaalThema toegevoegd.

---
+++ Taijitan_Yoshin_Ryu_vzw/Models/Domain/Lesmateriaal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taijitan_Yoshin_Ryu_vzw.Models.Domain
+{
+    public class Lesmateriaal
+    {
+        #region properties
+
+        public int Id { get; set; }
+        public string InhoudTekst { get; set; }
+        public string InhoudAfbeeldingen { get; set; }
+        public string InhoudVideo { get; set; }
+
+        #endregion
+
+        #region constructor
+
+        public Lesmateriaal(string InhoudTekst, string InhoudAfbeeldingen, string InhoudVideo)
+        {
+            this.InhoudTekst = InhoudTekst;
+            this.InhoudAfbeeldingen = InhoudAfbeeldingen;
+            this.InhoudVideo = InhoudVideo;
+        }
+
+        #endregion
+
+        #region methods
+
+        public ICollection<string> geefAlleAfbeeldingen()
+        {
+            return this.InhoudAfbeeldingen.Split(',');
+        }
+
+        #endregion
+    }
+}

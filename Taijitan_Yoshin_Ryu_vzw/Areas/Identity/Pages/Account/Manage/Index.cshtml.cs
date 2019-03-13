fe9b8412d5e4189b9ff3408749692fd8bb6bbcfa
Hans van der Staak <hans.vanderstaak@student.hogent.be>
2019-03-13 15:10:02 +0100

    identity manage wijzigen

--- Taijitan_Yoshin_Ryu_vzw/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Taijitan_Yoshin_Ryu_vzw/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,55 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 using Taijitan_Yoshin_Ryu_vzw.Data;
 using Taijitan_Yoshin_Ryu_vzw.Data.Repositories;
 using Taijitan_Yoshin_Ryu_vzw.Models.Domain;
 
-namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage
-{
-    public partial class IndexModel : PageModel
-    {
+namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage {
+    public partial class IndexModel : PageModel {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly IEmailSender _emailSender;
         private readonly GebruikerRepository _gebruikers;
 
         public IndexModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
-            IEmailSender emailSender,
-            ApplicationDbContext dbContext)
-        {
+            ApplicationDbContext dbContext) {
             _userManager = userManager;
             _signInManager = signInManager;
-            _emailSender = emailSender;
             _gebruikers = new GebruikerRepository(dbContext);
         }
- 
+
         [TempData]
         public string StatusMessage { get; set; }
 
         [BindProperty]
         public InputModel Input { get; set; }
 
-        public class InputModel
-        {
-            #region required inputs
-            [Required(ErrorMessage = "{0} is verplicht om in te vullen.")]
-            [Display(Name = "Gebruikersnaam")]
-            public string Username { get; set; }
+        public class InputModel {
+            #region verplichteVelden
+            //[Required(ErrorMessage = "{0} is verplicht om in te vullen.")]
+            //[Display(Name = "Gebruikersnaam")]
+            //public string Username { get; set; }
+
             [Required(ErrorMessage = "{0} is verplicht om in te vullen.")]
             [Display(Name = "E-mailadres")]
             [EmailAddress]
             public string Email { get; set; }
+
             [Required(ErrorMessage = "{0} is verplicht om in te vullen.")]
             [Display(Name = "Geboortedatum")]
             [DataType(DataType.Date)]
@@ -75,20 +67,13 @@ namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage
 
             [Required(ErrorMessage = "{0} is verplicht om in te vullen.")]
             public string Voornaam { get; set; }
+
             [Required(ErrorMessage = "{0} is verplicht om in te vullen.")]
             public string GsmNummer { get; set; }
             #endregion
 
-            #region disabled input fields            
-            public char Geslacht { get; set; }
-            public string GeboorteStad { get; set; }
-            public string GeboorteLand { get; set; }
-            public string RijksregisterNummer { get; set; }
-            public DateTime InschrijvingsDatum { get; set; }
-            #endregion
-                       
+            #region nietVerplichteVelden
             [Display(Name = "Telefoonnummer")]
-            //[Phone]
             public string TelefoonNummer { get; set; } //Niet verplicht
 
             [Display(Name = "E-mailadres van ouders")]
@@ -99,19 +84,26 @@ namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage
 
             [Display(Name = "Ik wens info te ontvangen over federale aangelegenheden.")]
             public bool InfoFederaleAangelegenheden { get; set; }
+            #endregion
+
+            #region onAanpasbaardeVelden            
+            public char Geslacht { get; set; }
+            public string GeboorteStad { get; set; }
+            public string GeboorteLand { get; set; }
+            public string RijksregisterNummer { get; set; }
+            public DateTime InschrijvingsDatum { get; set; }
+            #endregion
         }
 
-        public async Task<IActionResult> OnGetAsync()
-        {
+        public async Task<IActionResult> OnGetAsync() {
             var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
+            if (user == null) {
                 return NotFound($"Kon gebruiker niet laden met ID '{_userManager.GetUserId(User)}'.");
             }
             //gebruiker uit de repository halen
-            Gebruiker gebruiker = _gebruikers.GetByEmail(user.Email);
+            Gebruiker gebruiker = _gebruikers.GetByUserName(user.UserName);
 
-            var username = gebruiker.Username;
+            //var username = gebruiker.Username;
             var email = await _userManager.GetEmailAsync(user);
             var naam = gebruiker.Naam;
             var voorNaam = gebruiker.Voornaam;
@@ -131,9 +123,8 @@ namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage
             var infoFederale = gebruiker.InfoFederaleAangelegenheden;
 
 
-            Input = new InputModel
-            {
-                Username = username,
+            Input = new InputModel {
+                //Username = username,
                 Email = email,
                 Naam = naam,
                 Voornaam = voorNaam,
@@ -156,36 +147,30 @@ namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
                 return Page();
             }
 
             var user = await _userManager.GetUserAsync(User);
-            Gebruiker gebruiker = _gebruikers.GetByEmail(user.Email);
+            Gebruiker gebruiker = _gebruikers.GetByUserName(user.UserName);
 
-            if (user == null)
-            {
+            if (user == null) {
                 return NotFound($"Kon gebruiker niet laden met ID '{_userManager.GetUserId(User)}'.");
             }
 
             var email = await _userManager.GetEmailAsync(user);
-            if (Input.Email != email)
-            {
+            if (Input.Email != email) {
                 var setEmailResult = await _userManager.SetEmailAsync(user, Input.Email);
-                var setUserNameResult = await _userManager.SetUserNameAsync(user, Input.Email);
-                if (!setEmailResult.Succeeded || !setUserNameResult.Succeeded)
-                {
-                    var userId = await _userManager.GetUserIdAsync(user); 
+                if (!setEmailResult.Succeeded) {
+                    var userId = await _userManager.GetUserIdAsync(user);
                     throw new InvalidOperationException($"Er vond een onbekende error plaats bij het aanpassen van het mailadres van de gerbuiker met het ID '{userId}'.");
                 }
                 gebruiker.Email = Input.Email;
-                
+
             }
 
-            gebruiker.Username = Input.Username;
+            //gebruiker.Username = Input.Username;
             gebruiker.Naam = Input.Naam;
             gebruiker.Voornaam = Input.Voornaam;
             //gebruiker.GeboorteDatum = Input.GeboorteDatum;
@@ -201,42 +186,11 @@ namespace Taijitan_Yoshin_Ryu_vzw.Areas.Identity.Pages.Account.Manage
             gebruiker.EmailOuders = Input.EmailOuders;
             gebruiker.InfoClubAangelegenheden = Input.InfoClubAangelegenheden;
             gebruiker.InfoFederaleAangelegenheden = Input.InfoFederaleAangelegenheden;
-               
+
             _gebruikers.SaveChanges();
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Uw profiel is aangepast";
             return RedirectToPage();
-        }
-       
-        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-
-            var userId = await _userManager.GetUserIdAsync(user);
-            var email = await _userManager.GetEmailAsync(user);
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
-                pageHandler: null,
-                values: new { userId = userId, code = code },
-                protocol: Request.Scheme);
-            await _emailSender.SendEmailAsync(
-                email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-            StatusMessage = "Verification email sent. Please check your email.";
-            return RedirectToPage();
-        }
+        }        
     }
 }
